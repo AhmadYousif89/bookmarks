e67e591861848e5fdb6ca2b8489f40351b24b6a0
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-11-17 01:36:25 +0200

    Refactor DashboardProvider; integrate session refetching and enhance data fetching logic to prevent race conditions.

--- src/app/dashboard/dashboard.context.tsx
+++ src/app/dashboard/dashboard.context.tsx
@@ -11,6 +11,7 @@ import {
 } from "react";
 import { useSearchParams } from "next/navigation";
 import { TagData } from "./dal/bookmarks.dal";
+import { useSession } from "../(auth)/lib/auth.client";
 
 type DState = {
   active?: number;
@@ -31,6 +32,12 @@ export function DashboardProvider({ children }: { children: React.ReactNode }) {
   const abortRef = useRef<AbortController | null>(null);
   const [state, setState] = useState<DState>({ loading: true, tags: [] });
   const [trigger, setTrigger] = useState(0); // to force refresh
+  const requestIdRef = useRef(0); // to track latest request and prevent race conditions
+  const { refetch } = useSession();
+
+  useEffect(() => {
+    refetch();
+  }, []);
 
   const qs = useMemo(() => searchParams.toString(), [searchParams]);
   // function to refresh the data on demand
@@ -42,6 +49,7 @@ export function DashboardProvider({ children }: { children: React.ReactNode }) {
     abortRef.current?.abort();
     const controller = new AbortController();
     abortRef.current = controller;
+    const currentRequestId = ++requestIdRef.current;
 
     setState((s) => ({ ...s, loading: true }));
     const base = "/api/dashboard/data";
@@ -52,9 +60,14 @@ export function DashboardProvider({ children }: { children: React.ReactNode }) {
         if (!res.ok) throw new Error("Failed to fetch data");
         return res.json() as Promise<DState>;
       })
-      .then(({ active, archived, tags }) => setState({ active, archived, tags, loading: false }))
+      .then(({ active, archived, tags }) => {
+        if (currentRequestId !== requestIdRef.current) return; // outdated response ingored
+        setState({ active, archived, tags, loading: false });
+      })
       .catch((err) => {
         if (err.name === "AbortError") return;
+        if (currentRequestId !== requestIdRef.current) return; // outdated response ingored
+        console.error("Error fetching dashboard data:", err);
         setState({ active: 0, archived: 0, tags: [], loading: false });
       });
 
